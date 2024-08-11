0a975ca2d725c59db309ea84490b525e329f78e9
Sanjaya-Samudra <sanjayasamudra2003@gmail.com>
2024-08-11 09:52:28 +0530

    initial commit

---
+++ Demo/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main {
+    public static void main(String[] args) {
+        Starter.main(args);
+    }
+}
---
+++ Demo/src/main/java/Starter.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+
+    public static void main(String[] args){
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/add_customer_form.fxml"))));
+        stage.show();
+    }
+
+}
